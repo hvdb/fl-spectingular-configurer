9fc52116f0e5a287cad0f2d7017b5bde1af85811
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-05-14 20:41:53 +0200

    added test for registerTasks

--- test/configurerSpec.js
+++ test/configurerSpec.js
@@ -4,14 +4,12 @@ var grunt = require('grunt');
 
 var configurer;
 /**
- *
+ * Test the configurer
  */
-describe('Configurer ', function () {
+describe('Configurer should ', function () {
 
     beforeEach(function () {
-
         configurer = require('./../tasks/configurer')(grunt, __dirname);
-
     });
 
     it('default config should be handled correctly', function () {
@@ -22,15 +20,12 @@ describe('Configurer ', function () {
         expect(config.jshint.options.jshintrc).toEqual('.jshintrc');
 
         configurer.init(config);
-
         expect(grunt.config.get('jshint')).toBeDefined();
         expect(grunt.config.get('jshint').options.jshintrc).toEqual('.jshintrc');
         expect(grunt.config.get('karma')).toBeDefined();
-
     });
 
     it('exceptions on the default config should reflect to the grunt config', function () {
-
         var jshint = {
             jshint: {
                 jshintrc: '<%= config.paths.config %>/.jshintrc',
@@ -50,37 +45,29 @@ describe('Configurer ', function () {
         expect(config.karma).toBeDefined();
         expect(config.jshint).toBeDefined();
         expect(config.jshint.options.jshintrc).toEqual('<%= config.paths.config %>/.jshintrc');
-
         expect(config.jshint.options.foo).toEqual('bar baz');
 
         configurer.init(_config, config);
-
         expect(grunt.config.get('jshint')).toBeDefined();
         expect(grunt.config.get('jshint').options.jshintrc).toEqual('/tmp/foo/.jshintrc');
         expect(grunt.config.get('karma')).toBeDefined();
-
     });
-
 
     it('default config should be handled correctly even less verbose', function () {
         var config = configurer.configure();
         configurer.init(config);
-
         expect(grunt.config.get('jshint')).toBeDefined();
         expect(grunt.config.get('jshint').options.jshintrc).toEqual('.jshintrc');
         expect(grunt.config.get('karma')).toBeDefined();
-
     });
 
-    it('should register a set of tasks', function() {
-        var tasks = ['jshint','karma'];
-        configurer.registerTask('taskname', tasks);
-
-
-
-        //expect(grunt.tasks.get('taskname')).toBeDefined();
-
+    it('should register a set of tasks', function () {
+        spyOn(grunt, 'registerTask');
+        var tasks = ['jshint', 'karma'];
+        var taskName = 'taskname';
+        configurer.registerTask(taskName, tasks);
+        expect(grunt.registerTask).toHaveBeenCalledWith(taskName, tasks);
+        expect(grunt.registerTask.mostRecentCall.args).toContain(tasks);
     })
-
 
 });
