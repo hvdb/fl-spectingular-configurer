057bb33801acce911cf743d1175d461d6149d10e
Henk van den Brink <Hendrik.van.den.brink@ing.nl>
2015-05-18 16:13:57 +0200

    More clarifying error message.

--- configurer.js
+++ configurer.js
@@ -54,7 +54,7 @@ function Configurer(grunt, _dirname) {
                             if (!config[key][option]) {
                                 config[key][option] = _config[key][option];                                
                             } else {
-                                grunt.fail.fatal('The configuration with key ' + key + ' and option' + option + ' is going to override the existing config.' + config[key][option] + ' This is not allowed. Bye.')
+                                grunt.fail.fatal('The configuration with key ' + key + ' and option' + option + ' is going to override the existing config. This is not allowed, override the existing config with options instead. Bye.')
                             }
                         });
                     } else {
